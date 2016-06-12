64922ea9444393debde86d317a9fde615aaa83af
Max Holder <mxhold@gmail.com>
2016-06-12 10:34:31 -0700

    Use form helper tags

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,6 @@
 class CartsController < ApplicationController
   def update
-    product = Product.find(params[:product_id])
+    product = Product.find(params[:cart][:product_id])
     current_cart.add_product(product)
     redirect_to :root
   end
